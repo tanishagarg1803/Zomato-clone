5c70773b164407b0528de9106a2cc7fe96dd7871
tanishagarg1803 <tanishagarg1995@gmail.com>
2021-10-05 02:38:21 +0530

    added food tab ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import HomeLayoutHOC from "./HOC/Home.Hoc";
 //components
 import Temp from "./Components/temp";
 import HomeLayout from "./Layout/Home.layout";
+import Master from "./Components/master";
 
 function App() {
   return (
     <>
       <HomeLayoutHOC path="/" exact component={Temp} />
+      <HomeLayoutHOC path="/:type" exact component={Master} />
     </>
   );
 }
