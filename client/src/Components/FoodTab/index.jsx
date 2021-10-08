64e2aafddb365091277004d1b87baca8c61f3ba5
tanishagarg1803 <tanishagarg1995@gmail.com>
2021-10-08 10:55:13 +0530

    fix changes

--- client/src/Components/FoodTab/index.jsx
+++ client/src/Components/FoodTab/index.jsx
@@ -10,25 +10,21 @@ const MobileTabs = () => {
             id: `delivery`,
             icon: <RiShoppingBag3Line />,
             name: "Delivery",
-            isActive: false,
         },
         {
-            id: `dining`,
+            id: "dining",
             icon: <IoFastFoodOutline />,
             name: "Dining Out",
-            isActive: false,
         },
         {
-            id: `night`,
+            id: "night",
             icon: <BiDrink />,
             name: "Night life",
-            isActive: false,
         },
         {
-            id: `nutri`,
+            id: "nutri",
             icon: <IoNutritionOutline />,
             name: "Nutrition",
-            isActive: false,
         },
     ]);
     const { type } = useParams();
@@ -53,33 +49,53 @@ const MobileTabs = () => {
 };
 
 const LargeTabs = () => {
+
+    const [allTypes, setAllTypes] = useState([
+        {
+            id: `delivery`,
+            imageDefault: "https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png",
+            imageActive: "https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png?output-format=webp",
+            name: "Delivery",
+            activeColor: "yellow",
+        },
+        {
+            id: "dining",
+            imageDefault: "https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png?output-format=webp",
+            imageActive: "https://b.zmtcdn.com/data/o2_assets/30fa0a844f3ba82073e5f78c65c18b371616149662.png",
+            name: "Dining Out",
+            activeColor: "blue",
+        },
+        {
+            id: "night",
+            imageDefault: "https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png?output-format=webp",
+            imageActive: "https://b.zmtcdn.com/data/o2_assets/855687dc64a5e06d737dae45b7f6a13b1616149818.png",
+            name: "Night life",
+            activeColor: "purple",
+        },
+        {
+            id: "nutri",
+            imageDefault: "https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png",
+            imageActive: "https://b.zmtcdn.com/data/o2_assets/0f6dcb1aef93fa03ea3f91f37918f3bc1616649503.png",
+            name: "Nutrition",
+            activeColor: "yellow",
+        },
+    ]);
+
+    const { type } = useParams();
+
     return (
         <>
             <div className="hidden lg:flex gap-14 container px-20 mx-auto">
-                <div className="flex items-center gap-3">
-                    <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png" alt="delivery" className="w-full h-full" />
-                    </div>
-                    <h3 className="text-xl text-gray-700 font-semibold">Delivery</h3>
-                </div>
-                <div className="flex items-center gap-3">
-                    <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png?output-format=webp" alt="delivery" className="w-full h-full" />
-                    </div>
-                    <h3 className="text- texxlt-gray-700 font-semibold">Dining Out</h3>
-                </div>
-                <div className="flex items-center gap-3">
-                    <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png?output-format=webp" alt="delivery" className="w-full h-full" />
-                    </div>
-                    <h3 className="text-xl text-gray-700 font-semibold">Nightlife</h3>
-                </div>
-                <div className="flex items-center gap-3">
-                    <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img src="https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png" alt="delivery" className="w-full h-full" />
-                    </div>
-                    <h3 className="text-xl text-gray-700 font-semibold">Nutrition</h3>
-                </div>
+                {allTypes.map((items) => (
+                    <Link to={`/${items.id}`}>
+                        <div className="flex items-center gap-3">
+                            <div className={`w-16 h-16 bg-${type === items.id ? items.activeColor : "gray"}-100 p-4 rounded-full`}>
+                                <img src={type === items.id ? items.imageActive : items.imageDefault} alt="image" className="w-full h-full" />
+                            </div>
+                            <h3 className={type === items.id ? "text-xl text-zomato-400" : "text-xl text-gray-700"}>{items.name}</h3>
+                        </div>
+                    </Link>
+                ))}
             </div>
         </>
     );
